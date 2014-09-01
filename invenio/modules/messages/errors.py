cc6baad7c84cb915fec9bbb389769db286822ac9
Konstantinos Kostis <konstantinos.kostis@cern.ch>
2014-09-01 15:33:26 +0200

    messages: refactoring, fixes and improvements
    * General refactoring and improvements including methods cleanup,
      API additions, oauth fix in REST, docstrings fixes.
    
    * Adds pagination to REST API.
    
    Signed-off-by: Konstantinos Kostis <konstantinos.kostis@cern.ch>
    Reviewed-by: Leonardo Rossi <leonardo.r@cern.ch>

--- invenio/modules/messages/errors.py
+++ invenio/modules/messages/errors.py
@@ -23,11 +23,62 @@
 
 
 class InvenioWebMessageError(Exception):
+
     """A generic error for WebMessage."""
-    def __init__(self, message):
+
+    def __init__(self, message="InvenioWebMessageError", code=400):
         """Initialisation."""
         self.message = message
+        self.code = code
 
     def __str__(self):
         """String representation."""
-        return repr(self.message)
+        return repr("Error message: %s , Error code: %s"
+                    % (self.message, self.code))
+
+
+class MessageNotFoundError(InvenioWebMessageError):
+
+    """Error indicating a message was not found."""
+
+    def __init__(self, message="MessageNotFoundError", code=400):
+        """Initialization."""
+        super(MessageNotFoundError, self).__init__(message, code)
+
+
+class MessageNotDeletedError(InvenioWebMessageError):
+
+    """Error indicating a message was not deleted."""
+
+    def __init__(self, message="MessageNotDeletedError", code=400):
+        """Initialization."""
+        super(MessageNotDeletedError, self).__init__(message, code)
+
+
+class MessageNotCreatedError(InvenioWebMessageError):
+
+    """Error indicating a message was not found."""
+
+    def __init__(self, message="MessageNotCreatedError", code=400):
+        """Initialization."""
+        super(MessageNotCreatedError, self).__init__(message, code)
+
+
+class MessagesNotFetchedError(InvenioWebMessageError):
+
+    """Error indicating a message was not found."""
+
+    def __init__(self, message="MessagesNotFetchedError", code=400):
+        """Initialization."""
+        super(MessagesNotFetchedError, self).__init__(message, code)
+
+
+class MessageValidationError(InvenioWebMessageError):
+
+    """Error indicating validation error."""
+
+    def __init__(self, message="MessageValidationError", code=400,
+                 error_list=[]):
+        """Initialization."""
+        super(MessageValidationError, self).__init__(message, code)
+        self.error_list = error_list
